cc220aebe99db2753b50649f68b80a4d5db665b0
Mike <mike706574@gmail.com>
2017-08-20 20:56:39 -0500

    Example.

---
+++ ExampleService.Tests/Program_IsPrimeShould.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using static ExampleService.Program;
+using Xunit;
+
+namespace ExampleService.Tests
+{
+    public class Program_IsPrimeShould
+    {
+        private readonly Program _program;
+
+        public Program_IsPrimeShould()
+        {
+            _program = new Program();
+        }
+
+        #region Sample_TestCode
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        [InlineData(1)]
+        public void ReturnFalseGivenValuesLessThan2(int value)
+        {
+            bool result = _program.IsPrime(value);
+
+            Assert.False(result, $"{value} should not be prime");
+        }
+        #endregion
+
+        [Theory]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(7)]
+        public void ReturnTrueGivenPrimesLessThan10(int value)
+        {
+            bool result = _program.IsPrime(value);
+
+            Assert.True(result, $"{value} should be prime");
+        }
+
+        [Theory]
+        [InlineData(4)]
+        [InlineData(6)]
+        [InlineData(8)]
+        [InlineData(9)]
+        public void ReturnFalseGivenNonPrimesLessThan10(int value)
+        {
+            bool result = _program.IsPrime(value);
+
+            Assert.False(result, $"{value} should not be prime");
+        }
+    }
+}
---
+++ ExampleService/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace ExampleService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+
+        public bool IsPrime(int candidate)
+        {
+            if (candidate < 2)
+            {
+                return false;
+            }
+
+            for (var divisor = 2; divisor <= Math.Sqrt(candidate); divisor++)
+            {
+                if (candidate % divisor == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
